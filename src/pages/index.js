6c710c0bfabab4947fda543cfe79fd7a22f45668
Lee Bridges <lbridges@360view.com>
2018-08-10 13:55:59 -0500

    4th card

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,19 +5,23 @@ import Card from '../components/card';
 
 const IndexPage = () => (
   <div className="cards">
-    <h2>Our Products</h2>
+    <h2>360 View Products</h2>
     <div className="cardGroup">
       <Card
-        title="360 View"
-        text="The Growth Platform"
+        title="The Growth Platform"
+        text="It's what we're working on! It's gonna be great!"
       />
       <Card
-        title="360 View"
-        text="The Original Platform"
+        title="The Original Platform"
+        text="We're also working on this, and it's already great!"
       />
       <Card
-        title="360 View"
-        text="The Mobile Platform"
+        title="The Mobile Platform"
+        text="You can do stuff while you're on the go."
+      />
+      <Card
+        title="The Next Big Thing"
+        text="I just needed a 4th box!"
       />
     </div>
   </div>
